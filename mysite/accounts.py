807fe30c7e2299aa2bae298728391540fd5ec0ac
nana1243 <hieun1243@gmail.com>
2021-05-08 13:39:23 +0900

    fea) accounts 추가

---
+++ mysite/accounts.py
@@ -0,0 +1,27 @@
+data = [
+    {"identify": "0006-0000000002", "amount": "120000"},
+    {"identify": "0006-0000000002", "amount": "5000"},
+    {"identify": "0006-0000000002", "amount": "5000"},
+    {"identify": "0006-0000000004", "amount": "5000"},
+]
+
+balance = [
+    {"identify": "0006-0000000002", "amount": "10000"},
+    {"identify": "0006-0000000004", "amount": "16000"},
+]
+
+
+def check_balance(account):
+    for i in balance:
+        if i["identify"] == account:
+            current_bal = i["amount"]
+            return current_bal
+
+
+for i in data:
+    current_bal = check_balance(i["identify"])
+    if float(i["amount"]) <= float(current_bal):
+        new_bal = float(current_bal) - float(i["amount"])
+        print(new_bal)
+    else:
+        print("No balance.")

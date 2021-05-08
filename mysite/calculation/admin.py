0d5eff9b100a498fafe32ffb4e5ef0e2db29aaed
nana1243 <hieun1243@gmail.com>
2021-05-08 13:39:55 +0900

    fea) model admin에 추가

--- mysite/calculation/admin.py
+++ mysite/calculation/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
-from mysite.calculation.models import Balances, GetData, TransferAmount
+from .models import Balances, GetData, TransferAmount
 
 admin.site.register(Balances)
 admin.site.register(GetData)

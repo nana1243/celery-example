300347041f672c488a8d9bf947ae91b81c2b78bc
nana1243 <hieun1243@gmail.com>
2021-05-08 12:52:22 +0900

    fea) register models

--- mysite/calculation/admin.py
+++ mysite/calculation/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+from mysite.calculation.models import Balances, GetData, TransferAmount
+
+admin.site.register(Balances)
+admin.site.register(GetData)
+admin.site.register(TransferAmount)
